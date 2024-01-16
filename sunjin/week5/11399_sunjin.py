f227d1d1b577fae3fdd178f3c8296c6d9a33cee0
dawncoding <sunnuri99@gmail.com>
2024-01-16 15:17:48 +0900

    mod: [BOJ] 11399_ATM 주석 수정
    mod: [BOJ] 1654_랜선 자르기 주석 설명 수정
    
    mod: repository 폴더 구조 변경

--- sunjin/week5/11399_sunjin.py
+++ sunjin/week5/11399_sunjin.py
@@ -6,9 +6,10 @@ Constraints: 1 <= N <= 1,000 / 1 <= P_i <= 1,000
 1. p 정렬
 2. p배열 누적합
 예) p = [1, 2, 3, 4, 5]
-i = 1일 때, prefixSum[1] = 2
-i = 2일 때, prefixSum[2] = prefixSum[1] + p[2] = 2 + 3 = 5
-i = 3일 때, prefixSum[3] = prefixSum[2] + p[3] = 5 + 4 = 9
+i = index
+i = 1일 때, prefixSum[1] = prefixSum[0] + p[1] = 1 + 2 = 3
+i = 2일 때, prefixSum[2] = prefixSum[1] + p[2] = 3 + 3 = 6 = 1 + 2 + 3
+i = 3일 때, prefixSum[3] = prefixSum[2] + p[3] = 6 + 4 = 10 = 1 + 2 + 3 + 4
 5. prefixSum 배열의 요소들의 sum -> 각 사람이 돈을 인출하는 데 필요한 시간의 합의 최솟값
 
 시간 복잡도: O(NlogN) + O(N) = O(NlogN)
