9a8593ef365ff4fc1c7698b739c5a4f878bbe399
김영은 <jessicak0828@gmail.com>
2024-01-17 19:51:19 +0900

    [BOJ] 4948_ 베르트랑 공준 / 실버2 / 시간초과2

--- youngen/week6/4948_youngen.js
+++ youngen/week6/4948_youngen.js
@@ -1,29 +1,29 @@
 const input = require('fs').readFileSync("youngen/week6/input.txt").toString().trim().split('\n') 
 const numbers = input.map((item)=>+item)
-const howManyDivisors = (number) => {
+const getDivisors = (number) => {
   const divisors = []
   let i = 1
-  while(i <= number){
+  while(i <= number/2){
     if(Number.isInteger(number/i)){
       divisors.push(i)
     }
     i++
   }
-  return divisors.length
+  return [...divisors,number]
 }
 numbers.forEach((number)=>{
   let sum = 0
   // 시작이 홀수일 때 
   if(Number.isInteger(number/2)){
     for(let i = number+1 ; i <= number*2 ; i+=2){
-      if(howManyDivisors(i)===2){
+      if(getDivisors(i).length===2){
         sum = sum +  1
       }
     }
   } else {
     //시작이 짝수일 때
     for(let i = number+1 ; i <= number*2 ; i++){
-      if(howManyDivisors(i)===2){
+      if(getDivisors(i).length===2){
         sum = sum +  1
       }
     }
