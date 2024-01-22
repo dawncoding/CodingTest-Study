863c8f3a7bc3d49e2c5a044ff823c325e45e8ebe
nagyum <wildanimal19@naver.com>
2024-01-23 01:28:52 +0900

    [BOJ] 4948_베르트랑 공준/ 실버2/X

--- nagyum/week6/4948.py
+++ nagyum/week6/4948.py
@@ -1,47 +1,19 @@
-"""from math import sqrt
-
+num=123456*2+1 #작거나 같은 값까지 계산해주기 위해
+num_list=[1]*num 
+for i in range(1,num):
+    if i ==1:
+        continue
+    for j in range (2, int(i**0.5)+1):
+        if i%j ==0:
+            num_list[i]=0 #나뉜 다면 소수가 아니니까 리스트 값을 1->0 으로 변경
+            break
+        
 while True:
     n=int(input())
     
-    if n ==0:
+    if n==0:
         break
-    
-    count=0
-    
-    for i in range (1,2*n+1):
-        if i ==1:
-            continue
-        if i==2:
-            count +=1
-            continue
-        else:
-            for j in range(2,int(i**0.5)+1):
-                if i %j==0:
-                    break
-                else:
-                    count +=1
-    print(count)
-    """
-
-from math import sqrt
-
-while True:
-    n = int(input())
-    
-    if n == 0:
-        break
-    
-    count = 0
-    
-    for i in range(n + 1, 2 * n + 1):
-        is_prime = True
-
-        for j in range(2, int(sqrt(i)) + 1):
-            if i % j == 0:
-                is_prime = False
-                break
-
-        if is_prime:
-            count += 1
-    
-    print(count)
+    prime=0
+    for i in range(n+1,2*n+1):
+        prime += num_list[i]
+    print(prime)
