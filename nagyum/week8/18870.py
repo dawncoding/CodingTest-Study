c159217da5dee0312e4df748d465c90663dd55a1
nagyum <wildanimal19@naver.com>
2024-02-05 22:39:13 +0900

    [BOJ] 18870_좌표압축/실버2/X

--- nagyum/week8/18870.py
+++ nagyum/week8/18870.py
@@ -3,5 +3,11 @@ input=sys.stdin.readline
 
 N=int(input())
 arr=list(map(int,input().split()))
-num=sorted(set(arr))
+
+num=sorted(list(set(arr)))
+dic=dict(zip(num,list(range(len(num)))))
+
+for x in arr:
+    print(dic[x])
+
 
