b14d8f5c49142ec0133d0674e8336e0c06b8209b
Park Nagyum <wildanimal19@naver.com>
2024-02-16 16:17:24 +0900

    [BOJ]9461_파도반수열/실버3/X

--- nagyum/week8/9461.py
+++ nagyum/week8/9461.py
@@ -1,6 +1,6 @@
 import sys
 input=sys.stdin.readline
-
+"""
 N=int(input())
 arr =[int(input()) for _ in range(N)]
 tri=[1,1,1,2,2] 
@@ -11,3 +11,15 @@ for i in range(5,max(arr)+1):
 for i in arr:
     print(tri[i])
 
+"""
+
+N=int(input())
+dp=[0]*100
+dp[0],dp[1],dp[2],dp[3],dp[4]=1,1,1,2,2
+
+for i in range(5,100):
+    dp[i] = dp[i-2]+dp[i-3]
+    
+for _ in range(N):
+    i=int(input())
+    print(dp[i])
