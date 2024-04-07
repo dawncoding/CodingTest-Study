14d747f73805585cdc1a0ed79e548abe88ddd1e2
nagyum <wildanimal19@naver.com>
2024-04-07 22:37:27 +0900

    [BOJ] 18258_큐 2 / 실버 4 / O

---
+++ nagyum/week17/18258.py
@@ -0,0 +1,33 @@
+import sys
+from collections import deque
+input=sys.stdin.readline
+
+N=int(input())
+queue=deque([])
+
+for i in range (N):
+    com=input().split()
+    if com[0] == 'push':
+        queue.append(com[1])
+    elif com[0] == 'pop':
+        if (len(queue)==0):
+            print('-1')
+        else:
+            print(queue.popleft())
+    elif com[0] == 'size':
+        print(len(queue))
+    elif com[0] == 'empty':
+        if (len(queue)==0):
+            print(1)
+        else:
+            print(0)
+    elif com[0] == 'front':
+        if (len(queue)==0):
+            print('-1')
+        else:
+            print(queue[0])
+    elif com[0] == 'back':
+        if (len(queue)==0):
+            print('-1')
+        else:
+            print(queue[-1])
