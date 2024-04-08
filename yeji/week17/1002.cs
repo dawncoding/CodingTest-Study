921bb7ce1bdef8d349d88de3078800404715fbb6
yeji-gim <77161592+yeji-gim@users.noreply.github.com>
2024-04-08 11:42:02 +0900

    [BOJ] 1002_터렛 / 실버 3 / X

--- yeji/week17/1002.cs
+++ yeji/week17/1002.cs
@@ -8,25 +8,27 @@ namespace baek2
     {
         public static void Main()
         {
-            string input = Console.ReadLine();
-            int answer = 0;
-            Stack<char> stack = new Stack<char>();
-            for(int i = 0; i<input.Length;i++)
+            int n = int.Parse(Console.ReadLine());
+            while (n > 0)
             {
-                if (input[i] == '(')
-                    stack.Push(input[i]);
-                else // ')'인 경우
-                {
-                    stack.Pop();
-                    if (input[i-1] == '(') // 레이저인 경우
-                        answer += stack.Count;
+                n--;
+                string[] s = Console.ReadLine().Split();
 
-                    else // 끝인 경우
-                        answer++;
-                }
+                int x1 = int.Parse(s[0].ToString());
+                int y1 = int.Parse(s[1].ToString());
+                int r1 = int.Parse(s[2].ToString());
+                int x2 = int.Parse(s[3].ToString());
+                int y2 = int.Parse(s[4].ToString());
+                int r2 = int.Parse(s[5].ToString());
+
+                int sub = Math.Abs(r1 - r2);
+                double d = Math.Pow(MathF.Pow(x2 - x1, 2) + MathF.Pow(y2 - y1, 2),0.5);
+
+                if (d == 0 && r2 == r1) Console.WriteLine(-1);
+                else if (d < r1 + r2 && d > sub) Console.WriteLine(2);
+                else if (r1 + r2 == d || sub == d) Console.WriteLine(1);
+                else Console.WriteLine(0);
             }
-            Console.Write(answer);
-             
         }
     }
 }
