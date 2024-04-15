e3fad6c037be4e1939c78765b70ebf1fdb98a69f
jekyoung <best970611@naver.com>
2024-04-15 13:40:17 +0900

    [BOJ] 1699_제곱수의 합 / 실버 2 / X

---
+++ jekyoung/week18/1699.py
@@ -0,0 +1,14 @@
+import sys
+
+N = int(sys.stdin.readline())
+
+# 각각의 수를 1^2 으로 표현할때가 가장 큰 횟수이기 때문에 인덱스와 동일하게 x로 초기화
+dp = [x for x in range(N+1)]
+
+for i in range(1,N+1):
+    for j in range(1,i):
+        if j*j > i :
+            break
+        if dp[i] > dp[i-j*j] + 1 :
+            dp[i] = dp[i-j*j] + 1
+print(dp[N])
